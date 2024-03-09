6898a9018f1e3202d4f0aeca6f76fca54bce66e8
faxraddin09 <cabbarlifexreddin0022gmail.com>
2024-03-09 11:37:11 +0400

    method11

---
+++ metod2/metod2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace metod2
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //Console.WriteLine(Detail("Fexi", 17 ));
+        //    Info("Fexi","Baku",17);
+        //}
+        //static void Info(string name, string adress, int age)
+        //{
+        //    Console.WriteLine($"{Detail(name,age)} adress:{adress}");
+        //}
+        //static string Detail(string name,int age)
+        //{
+        //    return $"name:{name}  age:{age}";
+        //}
+        
+
+    }
+}
+//2ci method yazirsan name,age qebul edib return etsin
---
+++ test1/test1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine(Sum(5,7,9));
+            //metod 3 deyer qebul etsin cemin qaytarsin
+            //ad soyad yas metodu 
+            Console.WriteLine(Info("Fexi","Cabbarli",17));
+            Console.WriteLine(Info("Ulvi","Filankesov",19));
+        }
+        static int Sum(int a, int b,int c)
+        {
+            return a + b + c;
+        }
+        static string Info(string a, string b, int c)
+        {
+            //    return a + " "+b + " "+c;
+            return $"{a} {b} {c}";
+
+        }
+
+    }
+}
